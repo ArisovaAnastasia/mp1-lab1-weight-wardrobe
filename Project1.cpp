62fb44724d1ff586c0363d7fd74c67a1ee6244c1
unknown <T1496@E.local>
2022-09-29 14:23:20 +0300

    add lab1

---
+++ Project1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main() {
+	setlocale(LC_ALL, "Russian");
+	int h, w, d;
+	float p_DVP = 0.00085, p_DSP = 0.00065, p_DEREVO = 0.0015;
+	float stenka, bokovina, krizka, dver, polka, massa;
+
+	scanf_s("%i,%i,%i", &h, &w, &d);
+	if (h >= 180 && h<=220 && w >= 80 && w<=120 && d >= 50 && d <= 90) 
+	{
+		stenka = h * w * 0.5 * p_DVP;
+		bokovina = 2 * h * d * 1.5 * p_DSP;
+		krizka = 2 * w * d * 1.5 * p_DSP;
+		dver = 2 * h * w * 1 * p_DEREVO;
+		polka = (h / 40) * d * w * 1.5 * p_DSP;
+		massa = stenka + bokovina + krizka + dver + polka;
+		printf("%.2f", massa);
+	}
+	else
+	{
+			printf("Неверные данные");
+	}
+}
