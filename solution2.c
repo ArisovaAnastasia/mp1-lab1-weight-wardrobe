9dbfa63970ff6db1bcd0466621a061769cc8ec8f
uf0logic <ufologic@protonmail.com>
2022-09-30 00:19:34 +0300

    Поменял float на int

---
+++ solution2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int height = 0, width = 0, depth = 0;
+    int backThick = 5, sideThick = 15, coverThick = 15, doorThick = 1; // В миллиметрах
+    long double tree = 690/1e9, dsp = 550/1e9, dvp = 400/1e9;
+
+    while ((height < 180 || height > 220) && (width < 80 || width > 120) && (depth < 50 || depth > 90)) {
+        printf("Enter the data: Height, Width, Depth\n");
+        scanf("%i%i%i", &height, &width, &depth);
+    }
+
+    // Переводим сантиметры в миллиметры
+    height *= 10;
+    width *= 10;
+    depth *= 10;
+
+    // Находим объёмы
+    int backMass = height * width * backThick;
+    int sideMass = height * depth * sideThick;
+    int coverMass = depth * width * coverThick;
+    int doorMass = height * width * doorThick;
+    int shelfMass = width * depth;
+    int shelfNum = height / 400;
+
+    long double mass = dvp * backMass + dsp * (2 * sideMass + 2 * coverMass + shelfNum * shelfMass) + tree * doorMass;
+    printf("%.2Lf kg/m^3", mass);
+
+    return 0;
+}
