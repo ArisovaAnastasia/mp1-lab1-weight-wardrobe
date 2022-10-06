6505991fdeedf0667caf4f6f299392f6b55ebf4b
Grudzin Konstantin <113104424+Konstantin-Grudzin@users.noreply.github.com>
2022-10-06 14:04:19 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,6 +1,5 @@
-﻿#define _CRT_SECURE_NO_WARNINGS_
+#define _CRT_SECURE_NO_WARNINGS_
 #include <stdio.h>
-#include<iostream>
 #include<cmath>
 
 
@@ -8,31 +7,30 @@
 
 int main()
 {
-	long double plot_DVP=950/(1e9), plot_DSP=550/(1e9);
+	long double plot_DVP = 950 / (1e9), plot_DSP = 550 / (1e9),plot_Der= 540 / (1e9);
 	int H, W, D;
 
-	printf("H W D:");
-	scanf_s("%d %d %d", &H, &W, &D);
+	do
+	{
+		printf("H W D:");
+		scanf_s("%d %d %d", &H, &W, &D);
+	} while (!(1800 <= H && H <= 2200) || !(800 <= W && W <= 1200) || !(500 <= D && D <= 900));
 	//All in mm
 	H *= 10; W *= 10; D *= 10;
 
-	if (!(1800 <= H && H <= 2200) || !(800 <= W && W <= 1200) || !(500 <= D && D <= 900))
-	{
-		printf("Incorrect Data");
-	}
+	
+	
 
 
 	int v_wall = H * W * 5;
 	int v_bok = H * D * 15 * 2;
 	int v_top = W * D * 15 * 2;
-	int v_door = H * W * 10;
-	int v_pol = (H / 815) * W * D * 15;
+	int v_door = H * W * 10*2;
+	int v_pol = (H / 415) * W * D * 15;
 
-	long double mas = long double(plot_DVP * v_wall + plot_DSP * (v_bok + v_top + v_door + v_pol));
+	long double mas = long double(plot_DVP * v_wall + plot_DSP * (v_bok + v_top + v_pol) + v_door*plot_Der);
 
-	printf("Weigth:%lg", mas);
+	printf("Weigth:%lg kg", mas);
 
 
 }
-
-
