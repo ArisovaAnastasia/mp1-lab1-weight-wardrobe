88a3e982da90ecfec7d0cb856f55d060f4f1558d
uf0logic <ufologic@protonmail.com>
2022-10-09 20:18:29 +0300

    Chess Issue Completed

--- chess.c
+++ chess.c
@@ -2,30 +2,63 @@
 #include <stdlib.h>
 
 int main() {
-    int x1 = 5, x2 = 2, y1 = 7, y2 = 3;
+    int x1, x2, y1, y2;
     int figure;
 
     do {
-        printf("Choose a figure:\n\n1 - King; 2- Queen; 3 - Slon; 4 - Kon; 5 - Ladya\n"); // Названия Фигур напрямую не переводятся
-        scanf("%d",&figure);
-    } while (figure < 1 || figure > 5);
+        printf("Choose a figure:\n1:King 2:Queen 3:Bishop 4:Knight 5:Rook >>> "); //3:Slon 4:Kon 5:Ladya
+        scanf("%d", &figure);
+        printf("\nEnter initial coordinates (X1,Y1) : ");
+        scanf("%d%d", &x1, &y1);
+        printf("\nEnter desired coordinates (X1,Y1) : ");
+        scanf("%d%d", &x2, &y2); printf("\n");
+    } while (figure < 1 || figure > 5 || x1 < 1 || x1 > 8 || y1 < 1 || y1 > 8 || x2 < 1 || x2 > 8 || y2 < 1 || y2 > 8);
 
-    switch(figure) {
+    switch (figure) {
         case 1:
-            if (abs(x1 - x2) == 1 || abs(y1 - y2) == 1) printf("King can be moved\n");
-            else printf("King can't move this way\n");
+            if (abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1) printf("King can be moved\n");
+            else {
+                printf("King can't move this way\n");
+                if (abs(x1 - x2) >= 1 && abs(y1 - y2) == abs(x1 - x2)) printf("Bishop can be moved\nQueen can be moved\n");
+                if ((abs(x1 - x2) == 0 && abs(y1 - y2) >= 1) || (abs(x1 - x2) >= 1 && abs(y1 - y2) == 0)) printf("Rook can be moved\nQueen can be moved\n");
+                if ((abs(x1 - x2) == 1 && abs(y1 - y2) == 2) || (abs(x1 - x2) == 2 && abs(y1 - y2) == 1)) printf("Knight can be moved\n");
+            }
+            break;
         case 2:
-            if ((abs(x1 - x2) >= 1 && abs(y1 - y2) >= 1)||(abs(x1 - x2) == 0 && abs(y1 - y2) >= 1)||(abs(x1 - x2) >= 1 && abs(y1 - y2) == 0)) printf("Queen can be moved\n");
-            else printf("Queen can't move this way\n");
+            if ((abs(x1 - x2) >= 1 && abs(y1 - y2) == abs(x1 - x2)) || (abs(x1 - x2) == 0 && abs(y1 - y2) >= 1) || (abs(x1 - x2) >= 1 && abs(y1 - y2) == 0)) printf("Queen can be moved\n");
+            else {
+                printf("Queen can't move this way\n");
+                if (abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1) printf("King can be moved\n");
+                if ((abs(x1 - x2) == 1 && abs(y1 - y2) == 2) || (abs(x1 - x2) == 2 && abs(y1 - y2) == 1)) printf("Knight can be moved\n");
+            }
+            break;
         case 3:
-            if (abs(x1 - x2) >= 1 && abs(y1 - y2) >= 1) printf("Slon can be moved\n");
-            else printf("Slon can't move this way\n");
+            if (abs(x1 - x2) >= 1 && abs(y1 - y2) == abs(x1 - x2)) printf("Bishop can be moved\n");
+            else {
+                printf("Bishop can't move this way\n");
+                if (abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1) printf("King can be moved\n");
+                if ((abs(x1 - x2) == 1 && abs(y1 - y2) == 2) || (abs(x1 - x2) == 2 && abs(y1 - y2) == 1)) printf("Knight can be moved\n");
+                if ((abs(x1 - x2) == 0 && abs(y1 - y2) >= 1) || (abs(x1 - x2) >= 1 && abs(y1 - y2) == 0)) printf("Rook can be moved\nQueen can be moved\n");
+            }
+            break;
         case 4:
-            if ((abs(x1 - x2) == 1 && abs(y1 - y2) == 3)||(abs(x1 - x2) == 3 && abs(y1 - y2) == 1)) printf("Kon can be moved\n");
-            else printf("Kon can't move this way\n");
+            if ((abs(x1 - x2) == 1 && abs(y1 - y2) == 2) || (abs(x1 - x2) == 2 && abs(y1 - y2) == 1)) printf("Knight can be moved\n");
+            else {
+                printf("Knight can't move this way\n");
+                if (abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1) printf("King can be moved\n");
+                if (abs(x1 - x2) >= 1 && abs(y1 - y2) == abs(x1 - x2)) printf("Bishop can be moved\nQueen can be moved\n");
+                if ((abs(x1 - x2) == 0 && abs(y1 - y2) >= 1) || (abs(x1 - x2) >= 1 && abs(y1 - y2) == 0)) printf("Rook can be moved\nQueen can be moved\n");
+            }
+            break;
         case 5:
-            if ((abs(x1 - x2) == 0 && abs(y1 - y2) >= 1)||(abs(x1 - x2) >= 1 && abs(y1 - y2) == 0)) printf("Ladya can be moved\n");
-            else printf("Ladya can't move this way\n");
+            if ((abs(x1 - x2) == 0 && abs(y1 - y2) >= 1) || (abs(x1 - x2) >= 1 && abs(y1 - y2) == 0)) printf("Rook can be moved\n");
+            else {
+                printf("Rook can't move this way\n");
+                if (abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1) printf("King can be moved\n");
+                if (abs(x1 - x2) >= 1 && abs(y1 - y2) == abs(x1 - x2)) printf("Bishop can be moved\nQueen can be moved\n");
+                if ((abs(x1 - x2) == 1 && abs(y1 - y2) == 2) || (abs(x1 - x2) == 2 && abs(y1 - y2) == 1)) printf("Knight can be moved\n");
+            }
+            break;
     }
 
     return 0;
