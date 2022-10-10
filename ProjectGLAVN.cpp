22746393f05c30ffe5b07a29343399cb533bc91d
IluKozlov <113104431+Ganstailu@users.noreply.github.com>
2022-10-10 18:28:28 +0300

    e
    good

--- ProjectGLAVN.cpp
+++ ProjectGLAVN.cpp
@@ -2,31 +2,35 @@
 #include <iostream>
 using namespace std;
 int main() {
-	setlocale(0, "");
-	int plotDSP, plotDVP, plotDerevo,colvopolok;
-	float h, w, d, m1, m2, m3, m4, m5, mfinal;
-	plotDSP = 750;
-	plotDVP = 800;
-	plotDerevo = 540;
-	printf("Введите высоту шкафа в см");
-	scanf_s("%f", &h);
-	printf("Введите ширину шкафа в см");
-	scanf_s("%f", &w);
-	printf("Введите глубину шкафа в см");
-	scanf_s("%f", &d);
-	colvopolok = h / 40;
-	if (h < 180 || h>220 || d < 50 || d>90 || w < 80 || w>120) {
-		printf("Не верно введены данные");
-	}
-	else {
-		m1 = 800 * (h / 100) * (w / 100) * 0.005;
-		m2 = 2 * 750 * (h / 100) * (d / 100) * 0.015;
-		m3 = 2 * 750 * (w / 100) * (d / 100) * 0.015;
-		m4 = 2 * 540 * (h / 100) * (w / 100) * 0.001;
-		m5 = colvopolok * 750 * 0.015 * (h / 100) * (w / 100) * (d / 100);
-		mfinal = m1 + m2 + m3 + m4 + m5;
-		printf("Масса шкафа в кг = %f\n", mfinal);
 
-	}
+	setlocale(0, "");
+	float plotDVP = 0.0008;
+	float plotDSP = 0.0006;
+	float plotWOOD = 0.0015;
+	int h = 0, w = 0, d = 0;
+	float mfinal = 0, m1 = 0, m2 = 0, m3 = 0, m4 = 0, m5 = 0;
+
+	do {
+		printf("Введите высоту в см(от 180 до 220): ");
+		scanf_s("%d", &h);
+	} while (h < 180 || h > 220);
+
+	do {
+		printf("Введите ширину в см(от 80 до 120): ");
+		scanf_s("%d", &w);
+	} while (w < 80 || w > 120);
+
+	do {
+		printf("Введите глубину в см(от 50 до 90): ");
+		scanf_s("%d", &d);
+	} while (d < 50 || d > 90);
+
+	m1 = plotDVP * h * w * 0.5; // масса задней стенки из ДВП
+	m2 = 2 * plotDSP * h * d * 1.5; // масса двух боковых из ДСП
+	m3 = 2 * plotDSP * w * d * 1.5; // масса верхней и нижней крышек из ДСП
+	m4 = plotWOOD * h * w; // масса двух дверей из дерева
+	m5 = plotDSP * w * d * 1.5; // масса внутреней полки в шкафу толщиной 1.5см
+	mfinal = m1 + m2 + m3 + m4 + m5*int(h / 41.5);
+	printf("Масса шкафа = %.2f кг\n", mfinal);
 	return 0;
 }
